b081169d2a5c894ccbac41a96693bb899f76f51a
Vishakha Gautam <156678713+Vishakha2431@users.noreply.github.com>
2026-05-11 19:50:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-//your JS code here. If required.
+let fontsize = document.getElementById("fontsize");
+let fontcolor = document.getElementById("fontcolor");
+
+// SAVE COOKIES
+function savePreferences() {
+
+    document.cookie = `fontsize=${fontsize.value}`;
+
+    document.cookie = `fontcolor=${fontcolor.value}`;
+}
+
+// LOAD COOKIES
+document.addEventListener("DOMContentLoaded", () => {
+
+    let cookies = document.cookie.split(";");
+
+    cookies.forEach((cookie) => {
+
+        let [key, value] = cookie.trim().split("=");
+
+        if (key === "fontsize") {
+
+            fontsize.value = value;
+        }
+
+        if (key === "fontcolor") {
+
+            fontcolor.value = value;
+        }
+    });
+});
